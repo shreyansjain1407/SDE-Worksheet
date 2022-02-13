95c6d66135dd688d95ab51cd1ae0884a79bddae0
Shreyans Jain <42056701+shreyansjain1407@users.noreply.github.com>
2022-02-13 10:45:13 -0500

    Update FindWinnerOfCircularGame.java

--- MS Extra/FindWinnerOfCircularGame.java
+++ MS Extra/FindWinnerOfCircularGame.java
@@ -4,15 +4,15 @@ class FindWinnerOfCircularGame {
         for(int i = 0; i < n; i++) {
             list.add(i);
         }
+
         int cur = 0;
         while(list.size() != 1){
             cur += k - 1;
             cur %= list.size();
             // System.out.println("Removed Friend: " + (list.get(cur)+1));
             list.remove(cur);
-
         }
-
+        
         return list.get(0) + 1;
     }
 }
